07cc76fb83eedd90ebbdf011cf72422d985ac8f0
victorcwyu <victorcwyu@gmail.com>
2020-03-02 19:44:55 +0000

    Added two new tables to the database and added a bunch of fake data to the tables

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,16 @@
+CREATE TABLE assignments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+CREATE TABLE assignment_submissions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_date DATE
+);
