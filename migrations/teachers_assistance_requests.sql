c4c4217ab31a46eeb06ce7a8bda88199750ef0a3
victorcwyu <victorcwyu@gmail.com>
2020-03-03 19:43:33 +0000

    (HAD TO DELETE DATA AND RESTART).  Added two new tables to the database and added a bunch of fake data to the tables

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -1,20 +1,19 @@
 CREATE TABLE teachers (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
-  start_date DATE, 
-  end_date DATE,
-  is_active BOOLEAN
+  name VARCHAR(255) NOT NULL,
+  is_active BOOLEAN DEFAULT TRUE,
+  start_date DATE,
+  end_date DATE
 );
-
-
 
 CREATE TABLE assistance_requests (
   id SERIAL PRIMARY KEY NOT NULL,
-  assignment_id INTEGER,
-  student_id INTEGER,
-  teacher_id INTEGER,
-  started_at DATE, 
-  completed_at DATE, 
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  created_at TIMESTAMP,
+  started_at TIMESTAMP,
+  completed_at TIMESTAMP,
   student_feedback TEXT,
   teacher_feedback TEXT
 );
